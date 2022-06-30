9a08aca4807071bd7be7ebb3215b80144f432cbd
Isaac Richards <andro951@gmail.com>
2022-06-30 12:35:56 -0700

    Minor updates for preview.

--- Common/Globals/RecipeEditor.cs
+++ Common/Globals/RecipeEditor.cs
@@ -375,13 +375,5 @@ namespace WeaponEnchantments.Common.Globals
                 }
             });
         }
-        public override void OnCraft(Item item, Recipe recipe)
-        {
-
-        }
-        public override void ConsumeItem(Recipe recipe, int type, ref int amount)
-        {
-            base.ConsumeItem(recipe, type, ref amount);
-        }
     }
 }
